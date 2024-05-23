cb4dfccc696659e21b8e58529d61371ea89926c6
IgnasVanagas <ignas.judo@gmail.com>
2024-05-23 10:41:25 +0300

    Style + text

--- script.js
+++ script.js
@@ -7,20 +7,50 @@ document.addEventListener('DOMContentLoaded', function () {
     const temperatureValue = document.getElementById('temperature-value');
     const humidityValue = document.getElementById('humidity-value');
 
-    co2Value.textContent = co2Slider.value;
-    temperatureValue.textContent = temperatureSlider.value;
-    humidityValue.textContent = humiditySlider.value;
+    const statusText = document.querySelector('p');
 
-    // If you want to make sliders interactive, uncomment the following code
-    // co2Slider.addEventListener('input', function () {
-    //     co2Value.textContent = co2Slider.value;
-    // });
+    function updateStatusText() {
+        const co2 = co2Slider.value;
+        const temperature = temperatureSlider.value;
+        const humidity = humiditySlider.value;
+        let statusMessages = [];
 
-    // temperatureSlider.addEventListener('input', function () {
-    //     temperatureValue.textContent = temperatureSlider.value;
-    // });
+        if (co2 > 1000) {
+            statusMessages.push('Jūsų darbo mikroklimatas turi per didelį co2 kiekį');
+        }
+        if (temperature > 24) {
+            statusMessages.push('Jūsų darbo mikroklimatas yra per šiltas');
+        }
+        if (temperature < 20) {
+            statusMessages.push('Jūsų darbo mikroklimatas per vėsus');
+        }
+        if (humidity > 60) {
+            statusMessages.push('Jūsų darbo mikroklimatas per drėgnas');
+        }
+        if (humidity < 40) {
+            statusMessages.push('Jūsų darbo mikroklimatas per sausas');
+        }
+        if (statusMessages.length === 0) {
+            statusMessages.push('Jūsų darbo mikroklimatas yra optimalus');
+        }
 
-    // humiditySlider.addEventListener('input', function () {
-    //     humidityValue.textContent = humiditySlider.value;
-    // });
+        statusText.textContent = statusMessages.join(' ir ');
+    }
+
+    updateStatusText();
+
+    co2Slider.addEventListener('input', function () {
+        co2Value.textContent = co2Slider.value;
+        updateStatusText();
+    });
+
+    temperatureSlider.addEventListener('input', function () {
+        temperatureValue.textContent = temperatureSlider.value;
+        updateStatusText();
+    });
+
+    humiditySlider.addEventListener('input', function () {
+        humidityValue.textContent = humiditySlider.value;
+        updateStatusText();
+    });
 });
